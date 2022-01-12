d3e4ec99d872cfbd4483188d30e00c3b7386f579
Jan Rodzon <rodzonjan@wp.pl>
2022-01-13 00:35:36 +0100

    Fix minio deployment name

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MINIO_HELM_DEPLOYMENT=$(helm list --filter=minio | awk '{print $1}' | tail -n 1)
-helm uninstall $MINIO_HELM_DEPLOYMENT
+MINIO_DEPLOYMENT_NAME="minio"
+helm uninstall $MINIO_DEPLOYMENT_NAME
 ./remove_port_forwarding.sh -a
 
 kubectl delete all --all
