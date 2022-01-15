e4c7dbd12e3ad40b99f919f843ebf3ee464687ef
sgrzegorz <grzegorzsroka2@gmail.com>
2022-01-15 18:52:54 +0100

    Repaired main.py

--- spark/main.py
+++ spark/main.py
@@ -10,13 +10,24 @@ spark = SparkSession.builder.getOrCreate()
 
 SOURCE_BUCKET='raw-data'
 TARGET_BUCKET="processed-data"
+ACCESS_KEY = "rootuser"
+SECRET_KEY = "rootpass123"
+
+minio_service_host = os.getenv("MINIO_SERVICE_HOST")
+minio_service_port = os.getenv("MINIO_SERVICE_PORT")
+host = f"{minio_service_host}:{minio_service_port}"
+# to test locally uncomment following line
+# host = f"localhost:9000"
+print(f'Data lake source {host}')
+
+
 
 def load_config(spark_context: SparkContext):
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.access.key', 'rootuser')
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'rootpass123')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.access.key', ACCESS_KEY)
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key', SECRET_KEY)
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.path.style.access', 'true')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://localhost:9000')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint', f'http://{host}')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.connection.ssl.enabled', 'false')
 
 
@@ -27,16 +38,8 @@ sc = spark.sparkContext
 print("Configuration of spark successfully finished")
 
 
-minio_service_host = os.getenv("MINIO_SERVICE_HOST")
-minio_service_port = os.getenv("MINIO_SERVICE_PORT")
-host = f"{minio_service_host}:{minio_service_port}"
-# to test locally uncomment following line
-# host = f"localhost:9000"
-print(f'Data lake source {host}')
 
-access_key = "rootuser"
-secret_key = "rootpass123"
-client = Minio(host, access_key=access_key, secret_key=secret_key, secure=False)
+client = Minio(host, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
 
 def process_data(filename):
@@ -56,6 +59,8 @@ def process_data(filename):
 
 
 objects = client.list_objects(SOURCE_BUCKET)
+print(f"Processing {len(objects)} from {SOURCE_BUCKET}")
+
 for obj in objects:
     #     print(obj._object_name)
     process_data(obj._object_name)
